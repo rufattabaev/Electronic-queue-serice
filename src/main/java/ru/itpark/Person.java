20ed49aaa2d97e7175b8782de593b3cf714f7ce6
coursar <coursar@gmail.com>
2019-10-15 11:25:41 +0300

    Update Person.java

--- src/main/java/ru/itpark/Person.java
+++ src/main/java/ru/itpark/Person.java
@@ -5,5 +5,13 @@ public class Person {
 
     public Person(String name) {
         this.name = name;
+    } 
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
